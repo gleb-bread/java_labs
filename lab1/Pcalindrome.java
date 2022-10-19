f952130b8c272bdd15ada3e6bbacca79fccc53ee
gleb-bread <glebhets123@gmail.com>
2022-10-19 08:07:15 +0300

    1lab

---
+++ lab1/Pcalindrome.java
@@ -0,0 +1,20 @@
+public class Pcalindrome {
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            String s = args[i];
+            System.out.println(reverseString(s) + ' ' + isPalindrome(s));
+        }
+    }
+
+    public static String reverseString(String s) {
+        String str = "";
+        for (int i = s.length() - 1; i >= 0; --i)
+            str += s.charAt(i);
+        return str;
+    }
+
+    public static boolean isPalindrome(String s) {
+        String str = reverseString(s);
+        return s.equals(str);
+    }
+}
